9c347ca7002fcc152fec3ed1a6bd9fdd43404d63
Yestin Johnson <yestinj@gmail.com>
2017-09-30 18:25:48 +0200

    Added script to fix Steam installation case issues on case sensitive Mac filesystems.

---
+++ steam_fix_case_script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+USER=$(whoami)
+cd "/Users/${USER}/Library/Application Support/Steam/Steam.AppBundle/Steam/Contents/MacOS/public"; ls *.res | while read line ; do file=$(echo $line | tr '[:upper:]' '[:lower:]'); mv $line $file; done
+cd "/Users/${USER}/Library/Application Support/Steam/Steam.AppBundle/Steam/Contents/MacOS/steam/cached/"; ls *.res | while read line ; do file=$(echo $line | tr '[:upper:]' '[:lower:]'); mv $line $file; done
+cd "/Users/${USER}/Library/Application Support/Steam/Steam.AppBundle/Steam/Contents/MacOS/friends"; ls *.res | while read line ; do file=$(echo $line | tr '[:upper:]' '[:lower:]'); mv $line $file; done
